c0de9f66eb9d487215afd2949bf69257b269c6d8
j.freiwald <j.freiwald@l-shop-team.de>
2025-07-24 12:39:15 +0200

    first steps with clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+fn main(){
+    let output = Command::new("powershell.exe")
+        .args(&["-Command", "Get-Clipboard"])
+        .output()
+        .unwrap();
+
+
+    let content = String::from_utf8(output.stdout).unwrap();
+    let sanitized = content.replace("meine-firma.com", "example.com");
+    println!("Clipboard Inhalt:");
+    println!("{}", sanitized);
 }
