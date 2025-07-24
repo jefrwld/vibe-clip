414d3e243e40d8771663cd68db6a0deefb088016
j.freiwald <j.freiwald@l-shop-team.de>
2025-07-24 14:22:44 +0200

    more playing with clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 use std::process::Command;
+use std::io::Write;
 
 fn main(){
+
+    let content = read_windows_clipboard();
+    let sanitized = content.replace("meine-firma.com", "example.com");
+    println!("Clipboard Inhalt:");
+    println!("{}", sanitized);
+
+    write_windows_clipboard(&sanitized);
+
+}
+
+
+fn read_windows_clipboard() -> String {
     let output = Command::new("powershell.exe")
         .args(&["-Command", "Get-Clipboard"])
         .output()
         .unwrap();
 
+    String::from_utf8(output.stdout).unwrap() /* es returned von allein ? */
+}
 
-    let content = String::from_utf8(output.stdout).unwrap();
-    let sanitized = content.replace("meine-firma.com", "example.com");
-    println!("Clipboard Inhalt:");
-    println!("{}", sanitized);
+fn write_windows_clipboard(content: &str){
+    let mut child = Command::new("clip.exe")
+        .stdin(std::process::Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    child.stdin.as_mut().unwrap().write_all(content.as_bytes()).unwrap();
+    child.wait().unwrap();
 }
